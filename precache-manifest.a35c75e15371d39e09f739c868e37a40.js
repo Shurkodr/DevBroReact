be3ea31c7b8e49108182d31d3ef1bca943e47022
Shurkodr <sashaglazunov2@gmail.com>
2020-01-24 09:45:58 +0200

    Updates

---
+++ precache-manifest.a35c75e15371d39e09f739c868e37a40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57782c8a50d5aba8a622c0dcd1c54338",
+    "url": "/DevBroReact/index.html"
+  },
+  {
+    "revision": "2581a30c04302bd8f326",
+    "url": "/DevBroReact/static/css/main.f97c3d0e.chunk.css"
+  },
+  {
+    "revision": "1de69dada574a5568eec",
+    "url": "/DevBroReact/static/js/2.3aa1ea16.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/DevBroReact/static/js/2.3aa1ea16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2581a30c04302bd8f326",
+    "url": "/DevBroReact/static/js/main.3a8ca1b9.chunk.js"
+  },
+  {
+    "revision": "1e9c417e4aa72a41936c",
+    "url": "/DevBroReact/static/js/runtime-main.67bb4c0f.js"
+  },
+  {
+    "revision": "77f8732af007d2cb86d4eb68bdaea433",
+    "url": "/DevBroReact/static/media/logo.77f8732a.svg"
+  },
+  {
+    "revision": "d9ef1a05e9d0f5dd62b60d5e3e2b985c",
+    "url": "/DevBroReact/static/media/mail.d9ef1a05.svg"
+  },
+  {
+    "revision": "8a6a6783cf4c3e1c056cbdf89a125a90",
+    "url": "/DevBroReact/static/media/skype.8a6a6783.svg"
+  },
+  {
+    "revision": "09298f5d94b64329e86893a5aa44badf",
+    "url": "/DevBroReact/static/media/stone-main.09298f5d.png"
+  },
+  {
+    "revision": "c5ce43f7296154529c324fde48f82d27",
+    "url": "/DevBroReact/static/media/twitter.c5ce43f7.svg"
+  },
+  {
+    "revision": "5dfdc4c4a454f5a764886ce95e3d0f6c",
+    "url": "/DevBroReact/static/media/we-are-diff-img.5dfdc4c4.png"
+  }
+]);
